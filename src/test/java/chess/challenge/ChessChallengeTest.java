f59282a91de17c845d2ac9f0385a26a6cc16c0a8
Marcio Bernardo <mabernardo@gmail.com>
2016-08-24 10:51:27 -0300

    Fixed the failing tests if running them on Windows.

--- src/test/java/chess/challenge/ChessChallengeTest.java
+++ src/test/java/chess/challenge/ChessChallengeTest.java
@@ -1,6 +1,6 @@
 package chess.challenge;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -10,28 +10,24 @@ import java.util.Queue;
 
 import org.junit.Test;
 
-import chess.challenge.ChessBoard;
-import chess.challenge.ChessChallenge;
-import chess.challenge.ChessPiece;
-import chess.challenge.Knight;
-import chess.challenge.Rock;
-
 public class ChessChallengeTest {
 
     @Test
     public void testMain() {
-        byte[] result = { 75, 46, 75, 10, 46, 46, 46, 10, 46, 82, 46, 10, 10, 75, 46, 46, 10, 46, 46, 82, 10, 75, 46,
-                46, 10, 10, 46, 46, 75, 10, 82, 46, 46, 10, 46, 46, 75, 10, 10, 46, 82, 46, 10, 46, 46, 46, 10, 75, 46,
-                75, 10, 10 };
+        String result = "K.K....R."
+                      + "K....RK.."
+                      + "..KR....K"
+                      + ".R....K.K";
         ByteArrayInputStream in = new ByteArrayInputStream("3 3 2 0 0 1 0".getBytes());
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         PrintStream ps = new PrintStream(out);
 
+        System.out.println(result.toString());
         System.setIn(in);
         System.setOut(ps);
 
         ChessChallenge.main(new String[] {});
-        assertArrayEquals(result, out.toByteArray());
+        assertTrue(result.equals(out.toString().replaceAll("\n", "").replaceAll("\r", "")));
 
         System.setIn(System.in);
         System.setOut(System.out);
@@ -39,13 +35,14 @@ public class ChessChallengeTest {
 
     @Test
     public void testPrintConfigurations() {
-        byte[] result = { 82, 46, 46, 46, 10, 46, 78, 46, 78, 10, 46, 46, 82, 46, 10, 46, 78, 46, 78, 10, 10, 46, 82,
-                46, 46, 10, 78, 46, 78, 46, 10, 46, 46, 46, 82, 10, 78, 46, 78, 46, 10, 10, 46, 46, 82, 46, 10, 46, 78,
-                46, 78, 10, 82, 46, 46, 46, 10, 46, 78, 46, 78, 10, 10, 46, 46, 46, 82, 10, 78, 46, 78, 46, 10, 46, 82,
-                46, 46, 10, 78, 46, 78, 46, 10, 10, 46, 78, 46, 78, 10, 82, 46, 46, 46, 10, 46, 78, 46, 78, 10, 46, 46,
-                82, 46, 10, 10, 78, 46, 78, 46, 10, 46, 82, 46, 46, 10, 78, 46, 78, 46, 10, 46, 46, 46, 82, 10, 10, 46,
-                78, 46, 78, 10, 46, 46, 82, 46, 10, 46, 78, 46, 78, 10, 82, 46, 46, 46, 10, 10, 78, 46, 78, 46, 10, 46,
-                46, 46, 82, 10, 78, 46, 78, 46, 10, 46, 82, 46, 46, 10, 10 };
+        String result = "R....N.N..R..N.N"
+                      + ".R..N.N....RN.N."
+                      + "..R..N.NR....N.N"
+                      + "...RN.N..R..N.N."
+                      + ".N.NR....N.N..R."
+                      + "N.N..R..N.N....R"
+                      + ".N.N..R..N.NR..."
+                      + "N.N....RN.N..R..";
 
         ChessBoard board = new ChessBoard(4, 4);
         Queue<ChessPiece> pieces = new ArrayDeque<>();
@@ -62,7 +59,7 @@ public class ChessChallengeTest {
 
         System.setOut(ps);
         ChessChallenge.printConfigurations(board, pieces);
-        assertArrayEquals(result, out.toByteArray());
+        assertTrue(result.equals(out.toString().replaceAll("\n", "").replaceAll("\r", "")));
 
         System.setOut(System.out);
     }
